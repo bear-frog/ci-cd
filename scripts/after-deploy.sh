34e7cc4dd863d4cdbdd18c24c9d283e3b2d185ae
201710787 <egnachio@naver.com>
2023-03-16 02:04:36 +0900

    ADDED

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/app
+REPOSITORY=/home/ubuntu/app/dist/ssr
 
-ls
-cd dist/ssr
+cd $REPOSITORY
 sudo npm i
 # 참조
 # https://unchae.tistory.com/entry/PM2-80-443%ED%8F%AC%ED%8A%B8-%EC%82%AC%EC%9A%A9
