7507fcd82d42db5384b23f0dc7e44b796804163a
201710787 <egnachio@naver.com>
2023-03-16 01:53:16 +0900

    ADDED

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,7 +2,7 @@
 REPOSITORY=/home/ubuntu/app
 
 cd dist/ssr
-npm i
+sudo npm i
 # 참조
 # https://unchae.tistory.com/entry/PM2-80-443%ED%8F%AC%ED%8A%B8-%EC%82%AC%EC%9A%A9
 authbind --deep pm2 start index.js --watch
