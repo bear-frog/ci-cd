2204e92587ae8fe988fbdd31d920d6568aa7f81c
201710787 <egnachio@naver.com>
2023-03-16 02:24:47 +0900

    ADDED

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,4 +5,4 @@ cd $REPOSITORY
 sudo npm i
 # 참조
 # https://unchae.tistory.com/entry/PM2-80-443%ED%8F%AC%ED%8A%B8-%EC%82%AC%EC%9A%A9
-authbind --deep pm2 start index.js --watch
+authbind --deep pm2 reload index.js --watch
